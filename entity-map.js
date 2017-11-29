2294e4b58fe409bfe605a7b41ce3bdc97539e231
Ismael Serrano <info@ismaserrano.com>
2017-11-29 18:17:02 +0100

    Add typical spanish chars HTML entities.

--- entity-map.js
+++ entity-map.js
@@ -6,6 +6,16 @@ var EntityMap = {
 	'apos': "'",
 	'laquo': '«',
 	'raquo': '»',
+	'aacute': 'á',
+	'eacute': 'é',
+	'iacute': 'í',
+	'oacute': 'ó',
+	'uacute': 'ú',
+	'Aacute': 'Á',
+	'Eacute': 'É',
+	'Iacute': 'Í',
+	'Oacute': 'Ó',
+	'Uacute': 'Ú',
 
 	// Math
 
